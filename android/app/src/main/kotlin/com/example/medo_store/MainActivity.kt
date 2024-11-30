3cd397e2309a7ee55eb42c1a3773ff27be0e915d
mohamed nasr <medonasr326@gmail.com>
2024-11-30 22:06:32 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/medo_store/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.medo_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MedoStore());
+}
+
+class MedoStore extends StatelessWidget {
+  const MedoStore({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const  MaterialApp();
+  }
+}
